de922c2862dc172459d26bad69ec5c4e57a2db63
M02laleague <9558@holbertonstudents.com>
2024-09-20 14:41:24 +0200

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with a private instance attribute 'size'.
+"""
+
+
+class Square:
+    """
+    Represents a square with a specific size.
+    """
+    def __init__(self, size):
+        """
+        Initializes the square with a specific size.
+        """
+        self.__size = size
